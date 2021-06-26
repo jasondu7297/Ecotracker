ccd4fb4f072cecfc9e1a14868afd31af7db7dca0
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2021-06-26 13:20:11 -0400

    Added 3D models, updated tracker

--- Ecotracker/main/static/js/test.js
+++ Ecotracker/main/static/js/test.js
@@ -1,9 +1,86 @@
-let map;
 
 function initMap() {
-  console.log('hi')
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+
+    // map 
+    var ottawa = new google.maps.LatLng(45.424721, -75.695000)
+
+    var mapOptions = {
+        zoom: 10, 
+        center: ottawa
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'),mapOptions)
+
+    //directions Renderer
+    var rendererOptions = {
+        map: map
+    }
+
+    var directionsRenderer = new google.maps.DirectionsRenderer(rendererOptions)
+
+    directionsRenderer.setMap(map);
+
+    //directions Service
+    var directionsService = new google.maps.DirectionsService();
+
+    //so that route is calculated when user changes origin/destination
+    const onChangePathHandler = function () {
+        calcRoute(directionsRenderer, directionsService)
+    };
+
+    //listens for change in origin/destination
+    document.getElementById('origin').addEventListener('change', onChangePathHandler)
+    document.getElementById('destination').addEventListener('change', onChangePathHandler)
+
 }
+
+// takes the user route and calculates the 2-3 best routes
+function calcRoute (directionsService, directionsRenderer) {
+
+  var request = {
+    origin: {
+      query: document.getElementById('origin').value,
+    },
+    destination: {
+      query: document.getElementById('destination').value,
+    },
+    travelMode: google.maps.TravelMode.DRIVING,
+    provideRouteAlternatives: true
+  }
+
+  directionsService.route(request, function(response, status) {
+    if (status == 'OK'){
+      // render map
+      directionsRenderer.setDirections(response);
+    }
+
+    else {
+      alert('Request failed due to ' + status)
+    }
+  })
+}
+  
+  // when user changes route
+  function alternateRoute (directionsService, directionsRenderer) {
+    var request = {
+      origin: {
+        query: document.getElementById('origin').value,
+      },
+      destination: {
+        query: document.getElementById('destination').value,
+      },
+      travelMode: google.maps.TravelMode.DRIVING,
+      provideRouteAlternatives: true
+    }
+  
+    directionsService.route(request, function(response, status){
+      if (status == 'OK'){
+        // sets route based off of route selected from dropdown
+        var routenum = document.getElementById('routeOption').value - 1
+        directionsRenderer.setDirections(response);
+        directionsRenderer.setRouteIndex(routenum);
+        
+      }
+  
+    })
+  }

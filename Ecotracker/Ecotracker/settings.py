cf846c4233501d39bb0ed7f0eeb52743e862d44f
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2021-06-26 23:32:28 -0400

    Heroku hosting commit

--- Ecotracker/Ecotracker/settings.py
+++ Ecotracker/Ecotracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s64*60ef#2=iabh2=u=p6d6e19drd4!r6mx3k(+1azz1d-us+)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyDgIWNVzeFWWr0ecFoBuEI-qDSyImx7cYk'
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
